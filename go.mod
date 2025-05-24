a42216c72740c5f475e8254e66ac9c13709db370
Shreyas100100 <shreyaskulkarni1729@gmail.com>
2025-05-24 21:01:05 +0530

    refactor: use .env for database settings

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.24.0
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1
-	gorm.io/driver/sqlite v1.5.7
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.26.1
 )
 
@@ -15,11 +17,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-sqlite3 v1.14.28 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
-	gorm.io/driver/postgres v1.5.11 // indirect
 )
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/shreyas100100/ExpenseTracker/internal/model"
+	"github.com/shreyas100100/ExpenseTracker/pkg/config"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,8 +16,13 @@ var (
 )
 
 func InitDB() {
+	cfg := config.LoadConfig()
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
+		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort, cfg.TimeZone,
+	)
+
 	var err error
-	dsn := "host=localhost user=postgres password=user dbname=expensetracker port=5432 sslmode=disable TimeZone=Asia/Kolkata"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logger.WithError(err).Fatal("Failed to connect to database")
